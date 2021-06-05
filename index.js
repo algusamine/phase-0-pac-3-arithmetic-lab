6380c46130a06bbae1331b29a39e4fd8d2007255
Amine <algusamine@gmail.com>
2021-06-05 17:17:06 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,50 @@
+
+number = 10; 
+function add() {
+    return number += 1;
+};
+
+function subtract() {
+    return number -= 2;
+};
+
+function multiply() {
+    return number *= 3;
+};
+
+function divide() {
+    return number /= 4;
+};
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+a = 551; 
+function increment(n) {
+    return n += 1;
+};
+
+function decrement(n) {
+    return n -= 1;
+};
+
+function makeInt(string) {
+    return parseInt(string, 10);
+};
+
+function preserveDecimal(n) {
+ return parseFloat (n);
+};
